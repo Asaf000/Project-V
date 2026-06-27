d8998647ab86d8401e6bfe0b7c36698652de6a49
Asaf000 <asafeqbal6@gmail.com>
2026-06-28 03:06:01 +0530

    Create _init_.py

---
+++ models/_init_.py
@@ -0,0 +1,16 @@
+"""
+----------------------------------------------------------
+Models Package
+E-Commerce Web Application
+Thinkora Labs
+----------------------------------------------------------
+"""
+
+from app import db
+
+from .user import User
+from .category import Category
+from .product import Product
+from .cart import Cart
+from .order import Order
+from .order_items import OrderItem
